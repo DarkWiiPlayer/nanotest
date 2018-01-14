9662e732c63cfcc2b2d97746062d2664b70fbfa1
Dennis Fischer <dennis.fischer.wasd@gmail.com>
2018-01-14 17:53:02 +0100

    Formatting

--- lib/nanotest.rb
+++ lib/nanotest.rb
@@ -1,99 +1,87 @@
 # vim: set noexpandtab:
-
 class Nanotest
-  def self.version
-    return [0, 3, 0]
-  end
+	def self.version
+		return [0, 3, 0]
+	end
 
-  def self.run(opts={},*args,&block)
-    test = new opts, &block
-    test.run *args
-  end
+	def self.run(opts={}, *args, &block)
+		new(opts, &block).run(*args)
+	end
 
-  def initialize(opts={}, &block)
-    @tests = []
-    @before = []
-    @after = []
-    @opts = opts
-    self.instance_eval &block if block
-  end
+	def initialize(opts={}, &block)
+		@tests = []
+		@before = []
+		@after = []
+		@opts = opts
+		self.instance_eval &block if block
+	end
 
-  def message
-    @opts[:message]
-  end
-
-  def add(arg=nil, *args, &block)
+	def add(arg=nil, *args, &block)
 		if block then
-			if arg.is_a? String then
-				add(arg, block)
-			else
-				add(block)
+			arg.is_a?(String) && add(arg, block) || add(block)
+		elsif arg.is_a? String and args[0].is_a? Proc then
+			@tests << [arg, args[0]]
+		elsif arg.is_a? self.class then
+			sub = -> (*largs) { arg.run(*args, *largs) == 0 }
+			add arg.message && [arg.message, sub] || sub
+		elsif arg.is_a? Proc then
+			@tests << [(opts :verbose) ? "Unnamed test case" : nil, arg]
+		elsif arg.is_a? Array then
+			add Hash[*arg.flatten]
+		elsif arg.is_a? Hash then
+			arg.each { |key, value| add key, value }
+		else
+			raise ArgumentError, "Wrong arguments provided: [#{[arg.class] + args.map { |e| e.class }}]"
+		end
+		return self
+	end; alias :define :add
+
+	def before(step=nil, &block)
+		@before << (block || step || raise(ArgumentError, "Neither block nor lambda provided"))
+	end; alias :setup :before
+
+	def after(step=nil, &block)
+		@after << (block || step || raise(ArgumentError, "Neither block nor lambda provided"))
+	end; alias :cleanup :after
+
+	def run(*args)
+		@before.each { |step| step.call(*args) }
+		failed = 0
+		tests = (opts :random) ? @tests.shuffle : @tests
+		tests.each_with_index do |test, i|
+			begin
+				result = test[1].call(*args)
+			rescue StandardError => e
+				result =
+					"#{test[0]}\nTest threw an exception (#{e.class}):\n" +
+					" -> #{e.message}\nBacktrace:\n" +
+					"#{e.backtrace.join "\n" }" +
+					"\n...this means your test is broken, go fix it!"
 			end
-    elsif arg.is_a? String and args[0].is_a? Proc then
-      @tests << [arg, args[0]]
-    elsif arg.is_a? self.class then
-      sub = -> (*largs) { arg.run(*args, *largs) == 0 }
-      add arg.message && [arg.message, sub] || sub
-    elsif arg.is_a? Proc then
-      @tests << [(opts :verbose) ? "Unnamed test case" : nil, arg]
-    elsif arg.is_a? Array then
-      add Hash[*arg.flatten]
-    elsif arg.is_a? Hash then
-      arg.each { |key, value| add key, value }
-    else
-			raise ArgumentError, "Wrong arguments provided: [#{arg.class}, #{args.map { |e| e.class }}]"
-    end
-    return self
-  end
-	alias :define :add
+			if result and not result.is_a? String then
+				notify(test[0],true,i) if opts :notify_pass, :verbose
+			else
+				notify(result || test[0],false,i) if (result or test[0]) and not opts :silent
+				failed += 1
+				return true if opts :abort_on_fail
+				break if opts :break_on_fail
+			end
+		end
+		@after.each { |step| step.call(failed,*args) }
+		raise NanoTestFailed,<<~EOM if opts :raise and failed>0
+			#{failed} test#{failed>1 && 's' || nil} did not pass (see message#{ failed>1 ? 's' : nil } above)
+		EOM
+		return failed
+	end
 
-  def before(step=nil, &block)
-    @before << (block || step || raise(ArgumentError, "Neither block nor lambda provided"))
-  end; alias :setup :before
+	def message() @opts[:message] end
 
-  def after(step=nil, &block)
-    @after << (block || step || raise(ArgumentError, "Neither block nor lambda provided"))
-  end; alias :cleanup :after
+	private
 
-  def run(*args)
-    @before.each { |step| step.call(*args) }
-    failed = 0
-    tests = (opts :random) ? @tests.shuffle : @tests
-    tests.each_with_index do |test, i|
-      begin
-        result = test[1].call(*args)
-      rescue StandardError => e
-        result =
-          "#{test[0]}\nTest threw an exception (#{e.class}):\n" +
-          " -> #{e.message}\nBacktrace:\n" +
-          "#{e.backtrace.join "\n" }" +
-          "\n...this means your test is broken, go fix it!"
-      end
-      if result and not result.is_a? String then
-        notify(test[0],true,i) if opts :notify_pass, :verbose
-      else
-        notify(result || test[0],false,i) if (result or test[0]) and not opts :silent
-        failed += 1
-        return true if opts :abort_on_fail
-        break if opts :break_on_fail
-      end
-    end
-    @after.each { |step| step.call(failed,*args) }
-    raise NanoTestFailed,<<~EOM if opts :raise and failed>0
-      #{failed} test#{failed>1 && 's' || nil} did not pass (see message#{ failed>1 ? 's' : nil } above)
-    EOM
-    return failed
-  end
+	def notify(msg, pass, i=0)
+		puts "#{@opts[:line_start]}Test #{pass ? "passed" : "failed"}: #{@opts[:prefix]}#{msg}"
+	end
 
-  private
-
-  def notify(msg, pass, i=0)
-    puts "#{@opts[:line_start]}Test #{pass ? "passed" : "failed"}: #{@opts[:prefix]}#{msg}"
-  end
-
-  def opts(*args)
-    return args.any? { |arg| @opts[arg] }
-  end
+	def opts(*args) args.any? { |arg| @opts[arg] } end
 end
-
 class NanoTestFailed < StandardError; end
--- lib/nanotest/eval.rb
+++ lib/nanotest/eval.rb
@@ -1,21 +1,21 @@
 # vim: set noexpandtab:
 
 class Nanotest
-  module Eval
-    module Helper
-      def self.raises?(expr, except=nil, b=binding, *args)
-        except = except || StandardError
-        begin
-          if expr.is_a? String then
-            eval(expr, b)
-          else
-            expr.call(*args)
-          end
-          return false
-        rescue except => e
-          return e
-        end
-      end
+	module Eval
+		module Helper
+			def self.raises?(expr, except=nil, b=binding, *args)
+				except = except || StandardError
+				begin
+					if expr.is_a? String then
+						eval(expr, b)
+					else
+						expr.call(*args)
+					end
+					return false
+				rescue except => e
+					return e
+				end
+			end
 
 			def self.run expr, binding, *args
 				if expr.is_a? Proc then
@@ -24,119 +24,119 @@ class Nanotest
 					return eval expr, binding
 				end
 			end
-    end
+		end
 
-    def self.truthy(expr=nil, opts={}, &block)
+		def self.truthy(expr=nil, opts={}, &block)
 			opts = expr if expr.is_a?(Hash)
 			expr = block if block.is_a? Proc
 			raise ArgumentError, <<~ERROR unless expr.is_a? Proc or expr.is_a? String
 				Expression is #{expr.class}, expected Proc or String
 			ERROR
-      message = opts[:message].to_s + "#{opts[:message] ? "\n" : nil}Expect `#{expr}` to be truthy"
-      [
-        message,
-        lambda do |*args|
-          res = Helper::run expr, opts[:binding], *args
-          !!res || message + ", got #{res}"
-        end
-      ]
-    end
+			message = opts[:message].to_s + "#{opts[:message] ? "\n" : nil}Expect `#{expr}` to be truthy"
+			[
+				message,
+				lambda do |*args|
+					res = Helper::run expr, opts[:binding], *args
+					!!res || message + ", got #{res}"
+				end
+			]
+		end
 
-    def self.falsey(expr=nil, opts={}, &block)
+		def self.falsey(expr=nil, opts={}, &block)
 			opts = expr if expr.is_a?(Hash)
 			expr = block if block.is_a? Proc
 			raise ArgumentError, <<~ERROR unless expr.is_a? Proc or expr.is_a? String
 				Expression is #{expr.class}, expected Proc or String
 			ERROR
-      message = opts[:message].to_s + "#{opts[:message] ? "\n" : nil}Expect `#{expr}` to be falsey"
-      [
-        message,
-        lambda do |*args|
-          res = Helper::run expr, opts[:binding], *args
-          !res || message + ", got #{res}"
-        end
-      ]
-    end
+			message = opts[:message].to_s + "#{opts[:message] ? "\n" : nil}Expect `#{expr}` to be falsey"
+			[
+				message,
+				lambda do |*args|
+					res = Helper::run expr, opts[:binding], *args
+					!res || message + ", got #{res}"
+				end
+			]
+		end
 
-    def self.equal(exp1, exp2, opts={})
-      message = opts[:message] || "`#{exp1}` should equal `#{exp2}`"
-      [
-        message,
-        lambda do |*args|
-          res1 = Helper::run exp1, Array(opts[:binding]).first, *args
-          res2 = Helper::run exp2, Array(opts[:binding]).last,  *args
-          res1==res2 || message + "\nExpected:\n#{res2.inspect}\nGot:\n#{res1.inspect}"
-        end,
-      ]
-    end
+		def self.equal(exp1, exp2, opts={})
+			message = opts[:message] || "`#{exp1}` should equal `#{exp2}`"
+			[
+				message,
+				lambda do |*args|
+					res1 = Helper::run exp1, Array(opts[:binding]).first, *args
+					res2 = Helper::run exp2, Array(opts[:binding]).last,	*args
+					res1==res2 || message + "\nExpected:\n#{res2.inspect}\nGot:\n#{res1.inspect}"
+				end,
+			]
+		end
 
-    def self.unequal(exp1, exp2, opts={})
-      message = opts[:message] || "`#{exp1}` should not equal `#{exp2}`"
-      [
-        message,
-        lambda do |*args|
-          res1 = Helper::run exp1, Array(opts[:binding]).first, *args
-          res2 = Helper::run exp2, Array(opts[:binding]).last,  *args
-          res1 != res2 || message + "\nBoth evaluate to\n#{res1}"
-        end,
-      ]
-    end
+		def self.unequal(exp1, exp2, opts={})
+			message = opts[:message] || "`#{exp1}` should not equal `#{exp2}`"
+			[
+				message,
+				lambda do |*args|
+					res1 = Helper::run exp1, Array(opts[:binding]).first, *args
+					res2 = Helper::run exp2, Array(opts[:binding]).last,	*args
+					res1 != res2 || message + "\nBoth evaluate to\n#{res1}"
+				end,
+			]
+		end
 
-    def self.succeeds(expr=nil, opts={}, &block)
+		def self.succeeds(expr=nil, opts={}, &block)
 			opts = expr if expr.is_a?(Hash)
 			expr = block if block.is_a? Proc
 			raise ArgumentError, <<~ERROR unless expr.is_a? Proc or expr.is_a? String
 				Expression is #{expr.class}, expected Proc or String
 			ERROR
-      message = opts[:message] || "Expression '#{expr}' should succeed without errors"
-      [
-        message,
-        lambda do |*args|
-          !Helper::raises?(expr, opts[:exception], opts[:binding], *args)
-        end,
-      ]
-    end
+			message = opts[:message] || "Expression '#{expr}' should succeed without errors"
+			[
+				message,
+				lambda do |*args|
+					!Helper::raises?(expr, opts[:exception], opts[:binding], *args)
+				end,
+			]
+		end
 
-    def self.fails(expr=nil, opts={}, &block)
+		def self.fails(expr=nil, opts={}, &block)
 			opts = expr if expr.is_a?(Hash)
 			expr = block if block.is_a? Proc
 			raise ArgumentError, <<~ERROR unless expr.is_a? Proc or expr.is_a? String
 				Expression is #{expr.class}, expected Proc or String
 			ERROR
-      message = opts[:message] || "'Expression #{expr}' should fail with an error"
-      [
-        message,
-        lambda do |*args|
-          Helper::raises?(expr, opts[:exception], opts[:binding], *args)
-        end ,
-      ]
-    end
+			message = opts[:message] || "'Expression #{expr}' should fail with an error"
+			[
+				message,
+				lambda do |*args|
+					Helper::raises?(expr, opts[:exception], opts[:binding], *args)
+				end ,
+			]
+		end
 
 		# FIXME
-    def self.maps(table, expr=nil, opts={}, &block)
+		def self.maps(table, expr=nil, opts={}, &block)
 			opts = expr if expr.is_a?(Hash)
 			expr = block if block.is_a? Proc
 			raise ArgumentError, <<~ERROR unless expr.is_a? Proc or expr.is_a? String
 				Expression is #{expr.class}, expected Proc or String
 			ERROR
-      message = opts[:message] || "`#{expr}` should evaluate each key to its corresponding value."
-      [
-        message,
-        lambda do |*_|
-          table.each do |args, value|
-            begin
-              result = Helper::run(expr, opts[:binding], *args)
-            rescue Exception => e
-              raise e if opts[:noraise]
-              result = e.class
-            end
-            unless result == value
-              return message+"\nIncorrectly mapped\n#{args.join(", ")}\nto\n#{result}\nexpected\n#{value}"
-            end
-          end
-          return true
-        end
-      ]
-    end
-  end
+			message = opts[:message] || "`#{expr}` should evaluate each key to its corresponding value."
+			[
+				message,
+				lambda do |*_|
+					table.each do |args, value|
+						begin
+							result = Helper::run(expr, opts[:binding], *args)
+						rescue Exception => e
+							raise e if opts[:noraise]
+							result = e.class
+						end
+						unless result == value
+							return message+"\nIncorrectly mapped\n#{args.join(", ")}\nto\n#{result}\nexpected\n#{value}"
+						end
+					end
+					return true
+				end
+			]
+		end
+	end
 end
